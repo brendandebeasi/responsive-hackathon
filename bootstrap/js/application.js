dcce807d765475a49ca629f15b0c077e50afbccb
Solan, Tsvika <Tsvika.Solan@NFL.com>
2013-02-19 18:56:18 -0800

    fixing non fluid row

--- bootstrap/js/application.js
+++ bootstrap/js/application.js
@@ -2,5 +2,12 @@ $(document).ready(function() {
     $("#slideToggle").pageslide({ direction: "right", modal: true });
     $('body').click(function() {
         $.pageslide.close();
-    })
+    });
+
+	$(".carousel-vertical").carouFredSel({
+		direction: "up",
+		scroll: "linear",
+		width: "variable"
+	});
+
 });
