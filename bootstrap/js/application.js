594afb787dfe01bd528a3940091c505d00028629
Solan, Tsvika <Tsvika.Solan@NFL.com>
2013-02-25 16:42:56 -0800

    cleanup

--- bootstrap/js/application.js
+++ bootstrap/js/application.js
@@ -13,11 +13,9 @@ $(document).ready(function() {
 	   });
 	});
 
-    // grab the initial top offset of the navigation 
-	var sticky_navigation_offset_top = $('#sticky_navigation').position().top;
 	
 	// our function that decides weather the navigation bar should have "fixed" css position or not.
-	var sticky_navigation = function(){
+	sticky_navigation = function(){
 		var scroll_top = $(window).scrollTop(); // our current vertical position from the top
 		
 		// if we've scrolled more than the navigation, change its position to fixed to stick to top, otherwise change it back to relative
@@ -28,12 +26,21 @@ $(document).ready(function() {
 		}   
 	};
 	
-	// run our function on load
-	sticky_navigation();
-	
-	// and run it again every time you scroll
-	$(window).scroll(function() {
+	$("#myCarousel").imagesLoaded(function(){
+		// grab the initial top offset of the navigation 
+		sticky_navigation_offset_top = $('#sticky_navigation').position().top;
+
+		// run our function on load
 		sticky_navigation();
+		
+		// and run it again every time you scroll
+		$(window).scroll(function() {
+			sticky_navigation();
+		});
+	});
+
+	$(window).resize(function(){
+		sticky_navigation_offset_top = $('#sticky_navigation').position().top;
 	});
 
 });
